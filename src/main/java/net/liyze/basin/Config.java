c72d3917d4d6a2a0012b28587df8c5ae8abca3fe
Liyze09 <a52990419@outlook.com>
2023-05-20 13:50:53 +0800

    ACCUMULATE

--- src/main/java/net/liyze/basin/Config.java
+++ src/main/java/net/liyze/basin/Config.java
@@ -2,27 +2,26 @@ package net.liyze.basin;
 
 import com.google.gson.GsonBuilder;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
 
-import static net.liyze.basin.Main.*;
+import static net.liyze.basin.Main.config;
 
 public class Config {
     public static Config cfg = new Config();
     public int taskPoolSize = Runtime.getRuntime().availableProcessors() + 1;
 
-    static void initConfig() {
+    static void initConfig() throws Exception {
         if (!config.exists()) {
             String gson = (new GsonBuilder().setPrettyPrinting().create()).toJson(cfg);
             try (Writer writer = new FileWriter(config.getPath())) {
                 writer.write(gson);
-            } catch (IOException e) {
-                LOGGER.error("Error when create config file: ", e);
             }
         }
         try (Reader reader = new FileReader(config.getPath())) {
             cfg = (new GsonBuilder().setPrettyPrinting().create()).fromJson(reader, Config.class);
-        } catch (IOException e) {
-            LOGGER.error("Error when load config file: ", e);
         }
     }
 }
