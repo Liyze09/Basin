d52506283a5879ce094914bfeb82601f8940f580
Liyze09 <a52990419@oulook.com>
2023-06-02 13:15:46 +0800

    ACCUMULATE

--- src/main/java/net/liyze/basin/core/commands/ListCommand.java
+++ src/main/java/net/liyze/basin/core/commands/ListCommand.java
@@ -4,6 +4,9 @@ import net.liyze.basin.core.Main;
 import net.liyze.basin.interfaces.Command;
 
 import java.util.List;
+import java.util.Map;
+
+import static net.liyze.basin.core.Main.LOGGER;
 
 /**
  * Print all command loaded.
@@ -13,8 +16,19 @@ import java.util.List;
 public class ListCommand implements Command {
     @Override
     public void run(List<String> args) {
+        LOGGER.info("Commands");
         for (String i : Main.commands.keySet()) {
             System.out.println(i);
+        }
+        LOGGER.info("Var");
+        for (Map.Entry<String, String> i : Main.publicVars.entrySet()) {
+            System.out.print(i.getKey() + " = ");
+            System.out.println(i.getValue());
+        }
+        LOGGER.info("Env");
+        for (Map.Entry<String, Object> i : Main.envMap.entrySet()) {
+            System.out.print(i.getKey() + " = ");
+            System.out.println(i.getValue());
         }
     }
 
