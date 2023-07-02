ac0106d038850d253bfaac1ea3035d730b96b974
Liyze09 <119054047+Liyze09@users.noreply.github.com>
2023-07-02 14:40:04 +0800

    Update Bytecode.java

--- src/main/java/bscript/Bytecode.java
+++ src/main/java/bscript/Bytecode.java
@@ -4,4 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public record Bytecode(int version, Tree syntax, String source, List<String> args) implements Serializable {
+  public Bytecode() {
+    
+  }
 }
