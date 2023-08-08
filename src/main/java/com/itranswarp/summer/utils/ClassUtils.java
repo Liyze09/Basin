ddb5a0ee313a63c44b4891bd9e9501aef5c89543
Liyze09 <a52990419@outlook.com>
2023-08-08 22:15:59 +0800

    2.1.2-hot

--- src/main/java/com/itranswarp/summer/utils/ClassUtils.java
+++ src/main/java/com/itranswarp/summer/utils/ClassUtils.java
@@ -15,20 +15,7 @@ public class ClassUtils {
 
 
     public static <A extends Annotation> A findAnnotation(Class<?> target, Class<A> annoClass) {
-        A a = target.getAnnotation(annoClass);
-        for (Annotation anno : target.getAnnotations()) {
-            Class<? extends Annotation> annoType = anno.annotationType();
-            if (!annoType.getPackageName().equals("java.lang.annotation")) {
-                A found = findAnnotation(annoType, annoClass);
-                if (found != null) {
-                    if (a != null) {
-                        throw new BeanDefinitionException("Duplicate @" + annoClass.getSimpleName() + " found on class " + target.getSimpleName());
-                    }
-                    a = found;
-                }
-            }
-        }
-        return a;
+        return target.getAnnotation(annoClass);
     }
 
     @Nullable
